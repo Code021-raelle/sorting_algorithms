72a5865cdcab770f386884803b2cd74f2ec79611
Code021-raelle <gabrielakinshola3@gmail.com>
2024-01-19 18:43:55 +0100

    .c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * bubble_sort - Function to perform bubble sort on an array
@@ -9,9 +10,11 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j, k;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -20,8 +23,8 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-				printf("After swap: ");
-				for (size_t k = 0; k < size; k++)
+				printf(" ");
+				for (k = 0; k < size; k++)
 				{
 					printf("%d ", array[k]);
 				}
