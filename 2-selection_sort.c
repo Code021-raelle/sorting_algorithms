ddc56df402a8f8d17a9c9c641118785e7d627558
Code021-raelle <gabrielakinshola3@gmail.com>
2024-01-22 14:05:08 +0100

    sort an array of integers

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,26 +2,30 @@
 
 /**
  * selection_sort - sorts an array of integers in an ascending order
+ *                  using the selection sort algorithm.
  *
- * @array: pointer to the array to be sorted
+ * @array: pointer to the first element of the array
  * @size: number of elements in the array
  */
 void selection_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		int min_idx = i;
-		
-		for (size_t j = i + 1; j < size; j++)
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
 		if (min_idx != i)
 		{
-			int temp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
+			int temp = array[min_idx];
+
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
 		}
 	}
 }
