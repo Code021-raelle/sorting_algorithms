f2fceba2a460605d3bfc10a88306195d0bed0e69
Code021-raelle <gabrielakinshola3@gmail.com>
2024-01-23 17:13:49 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,20 +4,19 @@
  * bubble_sort - Function to perform bubble sort on an array
  * @array: Pointer to the array to be sorted
  * @size: Size of the array
- *
- * Return: Nothing
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-
+	int temp;
+	
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
+				temp = array[j];
 
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
